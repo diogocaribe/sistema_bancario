601277f5fca699b8d0d94587dea0e5d99b0fd6f1
Diogo Caribé <dogosousa@gmail.com>
2024-09-07 06:35:42 -0300

    Ruff format

--- sistema_bancario/app.py
+++ sistema_bancario/app.py
@@ -11,19 +11,20 @@ from sistema_bancario import (
 while True:
     opcao = input(menu())
 
-    if opcao == "d":
-        valor = float(input("\n Valor do depósito R$: "))
+    if opcao == 'd':
+        valor = float(input('\n Valor do depósito R$: '))
         depositar(
-            valor, exibir_saldo(historico_transacao),
+            valor,
+            exibir_saldo(historico_transacao),
             exibir_extrato(historico_transacao),
         )
         continue
 
-    if opcao == "s":
+    if opcao == 's':
         sacar()
 
-    if opcao == "e":
+    if opcao == 'e':
         exibir_extrato()
 
-    if opcao == "q":
+    if opcao == 'q':
         break
